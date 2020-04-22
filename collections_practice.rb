efb374dd1a364f60225882fe050f0aff19a3f554
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-22 12:30:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
-
+def sort_array_asc(array)
+  array.sort
+end
+def sort_array_desc(array)
+  array.sort.reverse
+end
+def sort_array_char_count(array)
+  result = array.sort{|left, right| left.length <=> right.length}
+end
+def swap_elements(array)
+  result = array.sort{|left, right| left[1] <=> right[2]}
+end
+def reverse_array(array)
+  new_array = []
+  new_array = array.reverse
+end
+def kesha_maker(array)
+  new_kesha_final = []
+    array.each do |name|
+      new_kesha = name.slice
+      puts new_kesha.inspect
+      new_kesha[2].replace "$"
+      new_kesha_final << new_kesha.join
+    end
+end
